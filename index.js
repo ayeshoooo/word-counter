de58e9c4466453b1570a5cf273ec841b2ad9fe3f
Ayeshoooo <112780976+ayeshoooo@users.noreply.github.com>
2023-02-01 13:40:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentences to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(`Your sentence word count is ${words.length}`);
---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answers : {
+    Sentence:string
+
+}= await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentences to count the word:"
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+console.log(`Your sentence word count is ${words.length}`)
